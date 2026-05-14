3f7702f3b1612277b89080d5c75d174666dfca93
Abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-05-14 16:23:17 +0100

    refactor: redesign home page layout with new title, welcome text, and image asset

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,55 @@ void main() {
 // materail app
 // scaffold
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: "Gestion de syndic",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-          brightness: Brightness.dark,
-
-        
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-          centerTitle: true,
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gestion des syndics"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Bienvenue",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Image.asset(
+              "assets/images/image-1.png",
+              height: 200,
+              width: 200,
+            ),
+            const Text(
+              "Application de Gestion de syndic",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              ),
+            )
           ],
         ),
       ),
