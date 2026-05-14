f4552b7ed89df36e0f8bff6d3fd33dbb8d6d6c95
Abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-05-14 13:06:13 +0100

    feat: add asset image support and refactor main screen layout with navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,92 +4,35 @@ void main() {
   runApp(const MyApp());
 }
 
+// stateless
+// materail app
+// scaffold
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.black,
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
+
+        
         ),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // First Box
-            Container(
-              height: 100,
-              width: 150,
-              margin: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.amberAccent,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Center(
-                child: Text(
-                  'First Box',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            
-            // Second Box
-            Container(
-              height: 100,
-              width: 150,
-              margin: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Center(
-                child: Text(
-                  'Hello from Paris!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white, // Changed to white so it's visible on a black background
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2,
-                    wordSpacing: 2,
-                  ),
-                ),
-              ),
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+          centerTitle: true,
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
           ],
         ),
       ),
